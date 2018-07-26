320d92e13e3a1c4d1603318ddfb9637ff18fa100
None <non@none.com>
2018-07-26 00:45:42 +0000

    Don't check subtrees at all

--- test/lint/git-subtree-check.sh
+++ test/lint/git-subtree-check.sh
@@ -10,66 +10,66 @@ if [ -z "$COMMIT" ]; then
     COMMIT=HEAD
 fi
 
-# Taken from git-subtree (Copyright (C) 2009 Avery Pennarun <apenwarr@gmail.com>)
-find_latest_squash()
-{
-	dir="$1"
-	sq=
-	main=
-	sub=
-	git log --grep="^git-subtree-dir: $dir/*\$" \
-		--pretty=format:'START %H%n%s%n%n%b%nEND%n' "$COMMIT" |
-	while read a b _; do
-		case "$a" in
-			START) sq="$b" ;;
-			git-subtree-mainline:) main="$b" ;;
-			git-subtree-split:) sub="$b" ;;
-			END)
-				if [ -n "$sub" ]; then
-					if [ -n "$main" ]; then
-						# a rejoin commit?
-						# Pretend its sub was a squash.
-						sq="$sub"
-					fi
-					echo "$sq" "$sub"
-					break
-				fi
-				sq=
-				main=
-				sub=
-				;;
-		esac
-	done
-}
+# # Taken from git-subtree (Copyright (C) 2009 Avery Pennarun <apenwarr@gmail.com>)
+# find_latest_squash()
+# {
+# 	dir="$1"
+# 	sq=
+# 	main=
+# 	sub=
+# 	git log --grep="^git-subtree-dir: $dir/*\$" \
+# 		--pretty=format:'START %H%n%s%n%n%b%nEND%n' "$COMMIT" |
+# 	while read a b _; do
+# 		case "$a" in
+# 			START) sq="$b" ;;
+# 			git-subtree-mainline:) main="$b" ;;
+# 			git-subtree-split:) sub="$b" ;;
+# 			END)
+# 				if [ -n "$sub" ]; then
+# 					if [ -n "$main" ]; then
+# 						# a rejoin commit?
+# 						# Pretend its sub was a squash.
+# 						sq="$sub"
+# 					fi
+# 					echo "$sq" "$sub"
+# 					break
+# 				fi
+# 				sq=
+# 				main=
+# 				sub=
+# 				;;
+# 		esac
+# 	done
+# }
 
-# find latest subtree update
-latest_squash="$(find_latest_squash "$DIR")"
-if [ -z "$latest_squash" ]; then
-    echo "ERROR: $DIR is not a subtree" >&2
-    exit 2
-fi
-set $latest_squash
-old=$1
-rev=$2
+# # find latest subtree update
+# latest_squash="$(find_latest_squash "$DIR")"
+# if [ -z "$latest_squash" ]; then
+#     echo "ERROR: $DIR is not a subtree" >&2
+#     exit 2
+# fi
+# set $latest_squash
+# old=$1
+# rev=$2
 
-# get the tree in the current commit
-tree_actual=$(git ls-tree -d "$COMMIT" "$DIR" | head -n 1)
-if [ -z "$tree_actual" ]; then
-    echo "FAIL: subtree directory $DIR not found in $COMMIT" >&2
-    exit 1
-fi
-set $tree_actual
-tree_actual_type=$2
-tree_actual_tree=$3
-echo "$DIR in $COMMIT currently refers to $tree_actual_type $tree_actual_tree"
-if [ "d$tree_actual_type" != "dtree" ]; then
-    echo "FAIL: subtree directory $DIR is not a tree in $COMMIT" >&2
-    exit 1
-fi
+# # get the tree in the current commit
+# tree_actual=$(git ls-tree -d "$COMMIT" "$DIR" | head -n 1)
+# if [ -z "$tree_actual" ]; then
+#     echo "FAIL: subtree directory $DIR not found in $COMMIT" >&2
+#     exit 1
+# fi
+# set $tree_actual
+# tree_actual_type=$2
+# tree_actual_tree=$3
+# echo "$DIR in $COMMIT currently refers to $tree_actual_type $tree_actual_tree"
+# if [ "d$tree_actual_type" != "dtree" ]; then
+#     echo "FAIL: subtree directory $DIR is not a tree in $COMMIT" >&2
+#     exit 1
+# fi
 
-# get the tree at the time of the last subtree update
-tree_commit=$(git show -s --format="%T" $old)
-echo "$DIR in $COMMIT was last updated in commit $old (tree $tree_commit)"
+# # get the tree at the time of the last subtree update
+# tree_commit=$(git show -s --format="%T" $old)
+# echo "$DIR in $COMMIT was last updated in commit $old (tree $tree_commit)"
 
 # # ... and compare the actual tree with it
 # if [ "$tree_actual_tree" != "$tree_commit" ]; then
